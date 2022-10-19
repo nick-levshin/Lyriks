4ce30b2c02726b4b5bf67be5c199ba9c49f8a786
Nick Levshin <nikita.levshin2016@gmail.com>
2022-10-19 18:42:45 +0300

    fix: change logo into link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,9 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLink to="/">
+          <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        </NavLink>
         <NavLinks />
       </div>
 
